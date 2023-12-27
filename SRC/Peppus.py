56570cc452a5372f0d2ace91541d02673cea5cb5
Alexis98ftv <alexis.dialop@gmail.com>
2023-12-27 19:47:02 +0000

    PEPPUS_WP1_v0.1

---
+++ SRC/Peppus.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+
+########################################################################
+# Peppus.py:
+# This is the Main Module of PEPPUS tool
+#
+#  Project:        PEPPUS
+#  File:           Peppus.py
+#  Date(YY/MM/DD): 01/02/22
+#
+#   Author: GNSS Academy
+#   Copyright 2021 GNSS Academy
+#
+# -----------------------------------------------------------------
+# Date       | Author             | Action
+# -----------------------------------------------------------------
+#
+# Usage:
+#   Peppus.py $SCEN_PATH
+########################################################################
+
+import sys, os
+
+# Update Path to reach COMMON
+Common = os.path.dirname(
+    os.path.abspath(sys.argv[0])) + '/COMMON'
+sys.path.insert(0, Common)
+
+# Import External and Internal functions and Libraries
+#----------------------------------------------------------------------
+from collections import OrderedDict
+from yaml import dump
+from COMMON import GnssConstants as Const
+from InputOutput import readConf
+from InputOutput import processConf
+from InputOutput import readRcvr
+from InputOutput import createOutputFile
+from InputOutput import readObsEpoch
+from InputOutput import generatePreproFile
+from InputOutput import PreproHdr
+from InputOutput import CSNEPOCHS, CSNPOINTS
+from Preprocessing import runPreProcMeas
+# from PreprocessingPlots import generatePreproPlots
+from COMMON.Dates import convertJulianDay2YearMonthDay
+from COMMON.Dates import convertYearMonthDay2Doy
+
+#----------------------------------------------------------------------
+# INTERNAL FUNCTIONS
+#----------------------------------------------------------------------
+
+def displayUsage():
+    sys.stderr.write("ERROR: Please provide path to SCENARIO as a unique argument\n")
+
+#######################################################
+# MAIN BODY
+#######################################################
+
+# Check InputOutput Arguments
+if len(sys.argv) != 2:
+    displayUsage()
+    sys.exit()
+
+# Extract the arguments
+Scen = sys.argv[1]
+
+# Select the Configuratiun file name
+CfgFile = Scen + '/CFG/peppus.cfg'
+
+# Read conf file
+Conf = readConf(CfgFile)
+# print(dump(Conf))
+
+# Process Configuration Parameters
+Conf = processConf(Conf)
+
+# Select the RCVR Positions file name
+RcvrFile = Scen + '/INP/RCVR/' + Conf["RCVR_FILE"]
+
+# Read RCVR Positions file
+RcvrInfo = readRcvr(RcvrFile)
+
+# Print header
+print( '------------------------------------')
+print( '--> RUNNING PEPPUS:')
+print( '------------------------------------')
+
+# Loop over RCVRs
+#-----------------------------------------------------------------------
+for Rcvr in RcvrInfo.keys():
+    # Display Message
+    print( '\n***-----------------------------***')
+    print( '*** Processing receiver: ' + Rcvr + '   ***')
+    print( '***-----------------------------***')
+
+    # Loop over Julian Days in simulation
+    #-----------------------------------------------------------------------
+    for Jd in range(Conf["INI_DATE_JD"], Conf["END_DATE_JD"] + 1):
+        # Compute Year, Month and Day in order to build input file name
+        Year, Month, Day = convertJulianDay2YearMonthDay(Jd)
+
+        # Compute the Day of Year (DoY)
+        Doy = convertYearMonthDay2Doy(Year, Month, Day)
+
+        # Display Message
+        print( '\n*** Processing Day of Year: ' + str(Doy) + ' ... ***')
+
+        # Define the full path and name to the OBS INFO file to read
+        ObsFile = Scen + \
+            '/INP/OBS/' + "OBS_%s_Y%02dD%03d.dat" % \
+                (Rcvr, Year % 100, Doy)
+
+        # If Preprocessing outputs are activated
+        if Conf["PREPRO_OUT"] == 1:
+            # Define the full path and name to the output PREPRO OBS file
+            PreproObsFile = Scen + \
+                '/OUT/PPVE/' + "PREPRO_OBS_%s_Y%02dD%03d.dat" % \
+                    (Rcvr, Year % 100, Doy)
+
+            # Create output file
+            fpreprobs = createOutputFile(PreproObsFile, PreproHdr)
+
+        # Initialize Variables
+        EndOfFile = False
+        ObsInfo = [None]
+        PrevPreproObsInfo = {}
+        for prn in range(1, Const.MAX_NUM_SATS_CONSTEL + 1):
+            PrevPreproObsInfo["G%02d" % prn] = {
+                "PrevEpoch": 86400,                                            # Previous SoD with measurements
+                "PrevL1": 0.0,                                                 # Previous L1 in cycles
+                "PrevL2": 0.0,                                                 # Previous L2
+                "PrevC1": 0.0,                                                 # Previous C1
+                "PrevP2": 0.0,                                                 # Previous C2
+                "PrevRej": 1,                                                  # Previous Rejection flag
+                
+                "CycleSlipBuffIdx": 0,                                         # Index of CS buffer
+                "CycleSlipFlagIdx": 0,                                         # Index of CS flag array
+                "GF_L_Prev": [0.0] * int(Conf["CYCLE_SLIPS"][CSNPOINTS]),      # Array with previous GF carrier phase observables
+                "GF_Epoch_Prev": [0.0] * int(Conf["CYCLE_SLIPS"][CSNPOINTS]),  # Array with previous epochs
+                "CycleSlipFlags": [0.0] * int(Conf["CYCLE_SLIPS"][CSNEPOCHS]), # Array with last cycle slips flags
+                
+                "PrevCode": Const.NAN,                                         # Previous Code
+                "PrevPhase": Const.NAN,                                        # Previous Phase in meters
+                "PrevCodeRate": Const.NAN,                                     # Previous Code Rate
+                "PrevPhaseRate": Const.NAN,                                    # Previous Phase Rate
+                "PrevStec": Const.NAN,                                         # Previous STEC
+                "PrevStecEpoch": Const.NAN,                                    # Previous STEC epoch
+                
+                "ResetAmb": 1,                                                 # Reset Ambiguities flag
+            } # End of SatPreproObsInfo
+
+        # Open OBS file
+        with open(ObsFile, 'r') as fobs:
+            # Read header line of OBS file
+            fobs.readline()
+
+            # LOOP over all Epochs of OBS file
+            # ----------------------------------------------------------
+            while not EndOfFile:
+
+                # If ObsInfo is not empty
+                if ObsInfo != []:
+
+                    # Read Only One Epoch
+                    ObsInfo = readObsEpoch(fobs)
+
+                    # If ObsInfo is empty, exit loop
+                    if ObsInfo == []:
+                        break
+
+                    # Preprocess OBS measurements
+                    # ----------------------------------------------------------
+                    PreproObsInfo = runPreProcMeas(Conf, RcvrInfo[Rcvr], ObsInfo, PrevPreproObsInfo)
+
+                    # If PREPRO outputs are requested
+                    if Conf["PREPRO_OUT"] == 1:
+                        # Generate output file
+                        generatePreproFile(fpreprobs, PreproObsInfo)
+
+                    # To be continued in next WP...
+
+                # End of if ObsInfo != []:
+
+                else:
+                    EndOfFile = True
+
+                # End of if ObsInfo != []:
+
+            # End of while not EndOfFile:
+
+        # End of with open(ObsFile, 'r') as f:
+
+        # If PREPRO outputs are requested
+        if Conf["PREPRO_OUT"] == 1:
+            # Close PREPRO output file
+            fpreprobs.close()
+
+            # Display Message
+            print("INFO: Reading file: %s and generating PREPRO figures..." %
+            PreproObsFile)
+
+            # Generate Preprocessing plots
+            # generatePreproPlots(PreproObsFile)
+
+    # End of JD loop
+
+# End of RCVR loop
+
+print( '\n------------------------------------')
+print( '--> END OF PEPPUS ANALYSIS')
+print( '------------------------------------')
+
+#######################################################
+# End of Peppus.py
+#######################################################
---
+++ SRC/Preprocessing.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python
+
+########################################################################
+# PETRUS/SRC/Preprocessing.py:
+# This is the Preprocessing Module of PEPPUS tool
+#
+#  Project:        PEPPUS
+#  File:           Preprocessing.py
+#  Date(YY/MM/DD): 01/02/21
+#
+#   Author: GNSS Academy
+#   Copyright 2021 GNSS Academy
+#
+# -----------------------------------------------------------------
+# Date       | Author             | Action
+# -----------------------------------------------------------------
+#
+########################################################################
+
+
+# Import External and Internal functions and Libraries
+#----------------------------------------------------------------------
+import sys, os
+
+# Add path to find all modules
+Common = os.path.dirname(os.path.dirname(
+    os.path.abspath(sys.argv[0]))) + '/COMMON'
+sys.path.insert(0, Common)
+from collections import OrderedDict
+from COMMON import GnssConstants as Const
+from InputOutput import RcvrIdx, ObsIdx, REJECTION_CAUSE
+from InputOutput import FLAG, VALUE, TH, CSNEPOCHS, CSNPOINTS, CSPDEGREE
+import numpy as np
+from COMMON.Iono import computeIonoMappingFunction
+
+# Preprocessing internal functions
+#-----------------------------------------------------------------------
+
+
+def runPreProcMeas(Conf, Rcvr, ObsInfo, PrevPreproObsInfo):
+    
+    # Purpose: preprocess GNSS raw measurements from OBS file
+    #          and generate PREPRO OBS file with the cleaned,
+    #          smoothed measurements
+
+    #          More in detail, this function handles:
+
+    #          * Measurements cleaning and validation and exclusion due to different 
+    #          criteria as follows:
+    #             - Minimum Masking angle
+    #             - Maximum Number of channels
+    #             - Minimum Carrier-To-Noise Ratio (CN0)
+    #             - Pseudo-Range Output of Range 
+    #             - Maximum Pseudo-Range Step
+    #             - Maximum Pseudo-Range Rate
+    #             - Maximum Carrier Phase Increase
+    #             - Maximum Carrier Phase Increase Rate
+    #             - Data Gaps checks and handling 
+    #             - Cycle Slips detection
+
+    #          * Build iono-free combination
+
+    # Parameters
+    # ==========
+    # Conf: dict
+    #         Configuration dictionary
+    # Rcvr: list
+    #         Receiver information: position, masking angle...
+    # ObsInfo: list
+    #         OBS info for current epoch
+    #         ObsInfo[1][1] is the second field of the 
+    #         second satellite
+    # PrevPreproObsInfo: dict
+    #         Preprocessed observations for previous epoch per sat
+    #         PrevPreproObsInfo["G01"]["C1"]
+
+    # Returns
+    # =======
+    # PreproObsInfo: dict
+    #         Preprocessed observations for current epoch per sat
+    #         PreproObsInfo["G01"]["C1"]
+
+    # Initialize output
+    PreproObsInfo = OrderedDict({})
+    
+    # Limit the satellites to the Number of Channels
+    # If the number of satellites in view exceeds the maximum allowed channels
+    # ----------------------------------------------------------
+    NSATS = len(ObsInfo)
+    index_array = []
+    if(NSATS > Conf["NCHANNELS_GPS"]):
+        lowest_elev = 100.0
+        lowest_index = 0
+        while(NSATS != Conf["NCHANNELS_GPS"]):
+            for index in range(0, len(ObsInfo) - 1):
+                if(index not in index_array):
+                    elev = float(ObsInfo[index][ObsIdx["ELEV"]])
+                    if(elev < lowest_elev):
+                        lowest_elev = elev
+                        lowest_index = index
+            index_array.append(lowest_index)
+            lowest_elev = 100.0
+            NSATS = NSATS - 1
+    
+    # Loop over satellites
+    for SatObs in ObsInfo:
+        # Initialize output info
+        SatPreproObsInfo = {
+            "Sod": 0.0,                         # Second of day
+            "Doy": 0,                           # Day of year
+            "Elevation": 0.0,                   # Elevation
+            "Azimuth": 0.0,                     # Azimuth
+            "C1": 0.0,                          # GPS L1C/A pseudorange
+            "L1": 0.0,                          # GPS L1 carrier phase (in cycles)
+            "L1Meters": 0.0,                    # GPS L1 carrier phase (in m)
+            "S1": 0.0,                          # GPS L1C/A C/No
+            "P2": 0.0,                          # GPS L2P pseudorange
+            "L2": 0.0,                          # GPS L2 carrier phase (in cycles)
+            "L2Meters": 0.0,                    # GPS L2 carrier phase (in m)
+            "S2": 0.0,                          # GPS L2 C/No
+            "IF_C": 0.0,                        # Iono-Free combination of codes
+            "IF_L": 0.0,                        # Iono-Free combination of phases
+            "Status": 1,                        # Measurement status
+            "RejectionCause": 0,                # Cause of rejection flag
+            "CodeRate": Const.NAN,              # Code Rate
+            "CodeRateStep": Const.NAN,          # Code Rate Step
+            "PhaseRate": Const.NAN,             # Phase Rate
+            "PhaseRateStep": Const.NAN,         # Phase Rate Step
+            "GF_L": Const.NAN,                  # Geometry-Free combination of phases in meters
+            "VtecRate": Const.NAN,              # VTEC Rate
+            "iAATR": Const.NAN,                 # Instantaneous AATR 
+            "Mpp": 0.0,                         # Iono Mapping
+            
+        } # End of SatPreproObsInfo
+
+        # Get satellite label
+        SatLabel = SatObs[ObsIdx["CONST"]] + "%02d" % int(SatObs[ObsIdx["PRN"]])
+
+        # Prepare outputs
+        # ----------------------------------------------------------
+        # Get SoD
+        SatPreproObsInfo["Sod"] = float(SatObs[ObsIdx["SOD"]])
+        # Get DoY
+        SatPreproObsInfo["Doy"] = int(SatObs[ObsIdx["DOY"]])
+        # Get Elevation
+        SatPreproObsInfo["Elevation"] = float(SatObs[ObsIdx["ELEV"]])
+        # Get Azimuth
+        SatPreproObsInfo["Azimuth"] = float(SatObs[ObsIdx["AZIM"]])
+        # Get GPS L1C/A pseudorange
+        SatPreproObsInfo["C1"] = float(SatObs[ObsIdx["C1"]])
+        # Get GPS L1 carrier phase (in cycles)
+        SatPreproObsInfo["L1"] = float(SatObs[ObsIdx["L1"]])
+        # Get GPS L1 carrier phase (in m)
+        SatPreproObsInfo["L1Meters"] = SatPreproObsInfo["L1"] * Const.GPS_L1_WAVE
+        # Get GPS L1C/A C/No
+        SatPreproObsInfo["S1"] = float(SatObs[ObsIdx["S1"]])
+        # Get GPS L2P pseudorange
+        SatPreproObsInfo["P2"] = float(SatObs[ObsIdx["P2"]])
+        # Get GPS L2 carrier phase (in cycles)
+        SatPreproObsInfo["L2"] = float(SatObs[ObsIdx["L2"]])
+        # Get GPS L2 carrier phase (in m)
+        SatPreproObsInfo["L2Meters"] = SatPreproObsInfo["L2"] * Const.GPS_L2_WAVE
+        # Get GPS L2 C/No
+        SatPreproObsInfo["S2"] = float(SatObs[ObsIdx["S2"]])     
+        
+
+        # Prepare output for the satellite
+        PreproObsInfo[SatLabel] = SatPreproObsInfo
+
+        # Limit the satellites to the Number of Channels
+        # ----------------------------------------------------------
+        if(len(index_array) != 0):
+            for index in index_array:
+                if(SatObs == ObsInfo[index]):
+                    SatPreproObsInfo["RejectionCause"] = REJECTION_CAUSE["NCHANNELS_GPS"]
+
+        # Check measurement data gaps
+        #-------------------------------------------------------------------------------
+        DeltaT = SatPreproObsInfo["Sod"] - PrevPreproObsInfo[SatLabel]["PrevEpoch"]
+        # DeltaT > 5000 simulates satellite's period to not tag non-visibility periods as data gaps
+        if(DeltaT > Conf["MAX_GAP"][1] and DeltaT > 5000):
+                # Check if gap detection is activated
+                if Conf["MAX_GAP"][0]:
+                    # Flag the measurement as a data gap
+                    SatPreproObsInfo["RejectionCause"] = REJECTION_CAUSE["DATA_GAP"]
+                    # Reset previous info
+                    PrevPreproObsInfo[SatLabel]["PrevCode"] = Const.NAN
+                    PrevPreproObsInfo[SatLabel]["PrevPhase"] = Const.NAN
+                    PrevPreproObsInfo[SatLabel]["PrevCodeRate"] = Const.NAN
+                    PrevPreproObsInfo[SatLabel]["PrevPhaseRate"] = Const.NAN
+                    PrevPreproObsInfo[SatLabel]["PrevStec"] = Const.NAN
+                    PrevPreproObsInfo[SatLabel]["PrevStecEpoch"] = Const.NAN
+
+
+        # Build measurement combinations
+        #-------------------------------------------------------------------------------
+        # Check that both Measurements exist at the same time
+        if(SatPreproObsInfo["C1"]>0.0 and SatPreproObsInfo["P2"]>0.0):
+            # Build Iono-free combination of codes and phases
+            #---------------------------------------------------------------------------
+            Pionofree=(SatPreproObsInfo["P2"]-Const.GPS_GAMMA_L1L2*SatPreproObsInfo["C1"])/(1-Const.GPS_GAMMA_L1L2)
+            Lionofree=(SatPreproObsInfo["L2Meters"]-Const.GPS_GAMMA_L1L2*SatPreproObsInfo["L1Meters"])/(1-Const.GPS_GAMMA_L1L2)
+
+            SatPreproObsInfo["IF_C"] = Pionofree
+            SatPreproObsInfo["IF_L"] = Lionofree
+
+            # Build Geometry-free combination of phases
+            #---------------------------------------------------------------------------
+            #Pgeomfree=(SatPreproObsInfo["C1"]-SatPreproObsInfo["P2"])/(1-Const.GPS_GAMMA_L1L2)
+            #Lgeomfree=(SatPreproObsInfo["L1Meters"]-SatPreproObsInfo["L2Meters"])/(Const.GPS_GAMMA_L1L2-1)
+
+            SatPreproObsInfo["GF_L"] = SatPreproObsInfo["L1Meters"] - SatPreproObsInfo["L2Meters"]
+
+        # Check satellite Elevation angle in front of the minimum by configuration
+        # ----------------------------------------------------------
+        if(SatPreproObsInfo["Elevation"] < Rcvr[RcvrIdx["MASK"]]):
+            SatPreproObsInfo["RejectionCause"] = REJECTION_CAUSE["MASKANGLE"]
+
+        # Measurement quality monitoring
+        #-------------------------------------------------------------------------------
+        # Check Signal To Noise Ratio in front of Minimum by configuration (if activated)
+        #-------------------------------------------------------------------------------
+        if(Conf["MIN_SNR"][0] == 1):
+            if(SatPreproObsInfo["S1"] < Conf["MIN_SNR"][1]):
+                SatPreproObsInfo["RejectionCause"] = REJECTION_CAUSE["MIN_SNR_L1"]
+            if(SatPreproObsInfo["S2"] < Conf["MIN_SNR"][1]):
+                SatPreproObsInfo["RejectionCause"] = REJECTION_CAUSE["MIN_SNR_L2"]
+
+        # Check Pseudo-ranges Out-of-Range in front of Maximum by configuration
+        #-------------------------------------------------------------------------------
+        if(Conf["MAX_PSR_OUTRNG"][0] == 1):
+            if(SatPreproObsInfo["C1"] > Conf["MAX_PSR_OUTRNG"][1]):
+                SatPreproObsInfo["RejectionCause"] = REJECTION_CAUSE["MAX_PSR_OUTRNG_L1"]
+            if(SatPreproObsInfo["P2"] > Conf["MAX_PSR_OUTRNG"][1]):
+                SatPreproObsInfo["RejectionCause"] = REJECTION_CAUSE["MAX_PSR_OUTRNG_L2"]
+
+
+
+
+
+
+
+
+
+        #[CHALLENGE] # Check Cycle Slips, if activated
+
+        
+
+
+
+
+
+
+
+        # Check Phase Rate (if activated) # MAX_PHASE_RATE 952.0
+        #-------------------------------------------------------------------------------
+        # Compute the Phase Rate in m/s
+        # cond != Const.NAN
+        SatPreproObsInfo["PhaseRate"] = (SatPreproObsInfo["L1Meters"] - PrevPreproObsInfo[SatLabel]["PrevPhase"]) / DeltaT
+        if(Conf["MAX_PHASE_RATE"][0] == 1):
+            # Check Phase Jump
+            if(SatPreproObsInfo["PhaseRate"] > Conf["MAX_PHASE_RATE"][1]):
+                # Reject the measurement setting flag: Max Phase Rate
+                SatPreproObsInfo["RejectionCause"] = REJECTION_CAUSE["MAX_PHASE_RATE"]
+
+           
+        # Check Phase Rate Step (if activated)
+        #-------------------------------------------------------------------------------
+        # Compute the Phase Rate Step in m/s2
+                #cond se pudo calcular y prev phaserate
+        SatPreproObsInfo["PhaseRateStep"] = (SatPreproObsInfo["PhaseRate"] - PrevPreproObsInfo[SatLabel]["PrevPhaseRate"]) / DeltaT
+        if(Conf["MAX_PHASE_RATE_STEP"][0] == 1):
+            # Check Phase Rate Jump
+            if(SatPreproObsInfo["PhaseRate"] > Conf["MAX_PHASE_RATE_STEP"][1]):
+                # Reject the measurement setting flag: Max Phase Rate Step
+                SatPreproObsInfo["RejectionCause"] = REJECTION_CAUSE["MAX_PHASE_RATE_STEP"]
+
+        # Check Code Rate detector (if activated)
+        #-------------------------------------------------------------------------------
+        # Compute the Code Rate in m/s as the first derivative of Raw Codes
+        SatPreproObsInfo["CodeRate"] = (SatPreproObsInfo["C1"] - PrevPreproObsInfo[SatLabel]["PrevCode"]) / DeltaT
+        if(Conf["MAX_CODE_RATE"][0] == 1):
+            # Check Code Jump
+            if(SatPreproObsInfo["CodeRate"] > Conf["MAX_CODE_RATE"][1]):
+                # Reject the measurement setting flag: Max Code Rate
+                SatPreproObsInfo["RejectionCause"] = REJECTION_CAUSE["MAX_CODE_RATE"]
+
+
+        # Check Code Rate Step detector (if activated)
+        #-------------------------------------------------------------------------------
+        # Compute the Code Rate step in m/s2 as the second derivative of Raw Codes
+                #cond se pudo calcular y prev coderate
+        SatPreproObsInfo["CodeRateStep"] = (SatPreproObsInfo["CodeRate"] - PrevPreproObsInfo[SatLabel]["PrevCodeRate"]) / DeltaT
+        
+
+        # Compute Iono Mapping Function as per MOPS Standard
+        #-------------------------------------------------------------------------------
+        SatPreproObsInfo["Mpp"] = computeIonoMappingFunction(SatPreproObsInfo["Elevation"])
+
+        # Check if previous observable is valid
+        #-------------------------------------------------------------------------------
+        if (PrevPreproObsInfo[SatLabel]["PrevStec"] != Const.NAN):
+            # Estimate STEC Gradient
+            DeltaSTEC = ((SatPreproObsInfo["GF_L"] - PrevPreproObsInfo[SatLabel]["PrevStec"]) / (1-Const.GPS_GAMMA_L1L2)) / DeltaT
+
+            # Estimate VTEC Gradient - VTECRate = DeltaVTEC
+            SatPreproObsInfo["VtecRate"] = DeltaSTEC / SatPreproObsInfo["Mpp"] 
+
+            # Compute instantaneous AATR
+            SatPreproObsInfo["iAATR"] = SatPreproObsInfo["VtecRate"] / SatPreproObsInfo["Mpp"]
+
+        #-------------------------------------------------------------------------------
+        # Set Status to 0 (invalid measurement) in case of rejection cause flag
+        #-------------------------------------------------------------------------------
+        if(SatPreproObsInfo["RejectionCause"] != 0):
+            SatPreproObsInfo["Status"] = 0
+        #-------------------------------------------------------------------------------
+        # Store current values into previous ones for next iteration
+        #-------------------------------------------------------------------------------
+        PrevPreproObsInfo[SatLabel]["PrevEpoch"] = SatPreproObsInfo["Sod"]
+        PrevPreproObsInfo[SatLabel]["PrevL1"] = SatPreproObsInfo["L1"]
+        PrevPreproObsInfo[SatLabel]["PrevL2"] = SatPreproObsInfo["L2"]
+        PrevPreproObsInfo[SatLabel]["PrevC1"] = SatPreproObsInfo["C1"]
+        PrevPreproObsInfo[SatLabel]["PrevP2"] = SatPreproObsInfo["P2"]
+        PrevPreproObsInfo[SatLabel]["PrevRej"] = SatPreproObsInfo["RejectionCause"]
+        # Cycle Slip
+
+        # Just for C1 and L1 (in meters) (to not duplicate all code in python)
+        PrevPreproObsInfo[SatLabel]["PrevCode"] = SatPreproObsInfo["C1"]
+        PrevPreproObsInfo[SatLabel]["PrevPhase"] = SatPreproObsInfo["L1Meters"]
+        # Derivatives
+        PrevPreproObsInfo[SatLabel]["PrevCodeRate"] = SatPreproObsInfo["CodeRate"]
+        PrevPreproObsInfo[SatLabel]["PrevPhaseRate"] = SatPreproObsInfo["PhaseRate"]
+        PrevPreproObsInfo[SatLabel]["PrevStec"] = SatPreproObsInfo["GF_L"]
+
+    return PreproObsInfo
+
+# End of function runPreProcMeas()
+
+########################################################################
+# END OF PREPROCESSING FUNCTIONS MODULE
+########################################################################
